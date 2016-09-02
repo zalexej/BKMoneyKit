c61d2db6047d445f42134ce10f9d5df6ad7fa3aa
Alexey Zarva <azarva@siber.com>
2016-09-02 13:46:41 +0300

    add setText

--- BKMoneyKit/BKCardNumberLabel.m
+++ BKMoneyKit/BKCardNumberLabel.m
@@ -120,4 +120,10 @@
     return self.cardNumberFormatter.cardPatternInfo.companyName;
 }
 
+- (void)setText:(NSString *)text
+{
+	[super setText:[self.cardNumberFormatter formattedStringFromRawString:text]];
+	[self updateCardLogoImage];
+}
+
 @end
